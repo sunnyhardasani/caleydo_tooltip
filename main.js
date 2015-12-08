1b182625014654413dabfacc79ec399dad41c521
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-12-08 11:25:11 +0100

    changed tooltip to typescript

--- main.js
+++ main.js
@@ -1,48 +1,40 @@
 /**
  * Created by Samuel Gratzl on 05.08.2014.
  */
-
-/**
- * a tooltip module, providing a single method: 'bind'
- */
-define(['exports', 'd3', 'css!./style'], function (exports, d3) {
-  // add the tooltip area to the webpage
-  var tooltip = d3.select('body').append('div')
-    .attr('class', 'tooltip')
-    .style('opacity', 0);
-
-  /**
-   * returns a D3 compatible call method, which registers itself to show a tooltip upon mouse enter
-   * @param toLabel the text to show or a function to determine the text to show
-   * @returns {Function}
-   */
-  function bind(toLabel) {
-    //wrap as function
-    toLabel = d3.functor(toLabel);
-
-    return function (selection) {
-      selection.on({
-        mouseenter: function (d, i) {
-          tooltip
-            .html(toLabel.call(this, d, i))
-            .style('left', (d3.event.pageX + 5) + 'px')
-            .style('top', (d3.event.pageY - 28) + 'px');
-          tooltip.style('display',null).transition()
-            .delay(200)
-            .duration(200)
-            .style('opacity', 0.9);
-        },
-        mouseleave: function () {
-          tooltip.transition()
-            .duration(200)
-            .style('opacity', 0)
-            .each('end', function() {
-              d3.select(this).style('display', 'none');
+define(["require", "exports", 'd3', "css!./style"], function (require, exports, d3) {
+    var tooltip = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('display', 'block')
+        .style('opacity', 0);
+    /**
+     * returns a D3 compatible call method, which registers itself to show a tooltip upon mouse enter
+     * @param toLabel the text to show or a function to determine the text to show
+     * @returns {Function}
+     */
+    function bind(toLabel) {
+        //wrap as function
+        var labelfor = d3.functor(toLabel);
+        return function (selection) {
+            selection.on('mouseenter', function (d, i) {
+                tooltip
+                    .html(labelfor.call(this, d, i))
+                    .style('left', (d3.event.pageX + 5) + 'px')
+                    .style('top', (d3.event.pageY - 28) + 'px');
+                tooltip.style('display', 'block').transition()
+                    .delay(200)
+                    .duration(200)
+                    .style('opacity', 0.9);
+            })
+                .on('mouseleave', function () {
+                tooltip.transition()
+                    .duration(200)
+                    .style('opacity', 0)
+                    .each('end', function () {
+                    d3.select(this).style('display', 'none');
+                });
             });
-        }
-      });
-    };
-  }
-
-  exports.bind = bind;
+        };
+    }
+    exports.bind = bind;
 });
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,44 @@
+/**
+ * Created by Samuel Gratzl on 05.08.2014.
+ */
+
+/// <reference path="../../tsd.d.ts" />
+/// <amd-dependency path='css!./style' />
+import d3 = require('d3');
+
+const tooltip = d3.select('body').append('div')
+  .attr('class', 'tooltip')
+  .style('display', 'block')
+  .style('opacity', 0);
+
+
+/**
+ * returns a D3 compatible call method, which registers itself to show a tooltip upon mouse enter
+ * @param toLabel the text to show or a function to determine the text to show
+ * @returns {Function}
+ */
+export function bind<T>(toLabel:((d:T, i:number)=>string) | string) {
+  //wrap as function
+  const labelfor:((d:T, i:number)=>string) = <any>d3.functor(toLabel);
+
+  return function (selection:d3.Selection<T>) {
+    selection.on('mouseenter', function (d:T, i) {
+        tooltip
+          .html(labelfor.call(this, d, i))
+          .style('left', (d3.event.pageX + 5) + 'px')
+          .style('top', (d3.event.pageY - 28) + 'px');
+        tooltip.style('display', 'block').transition()
+          .delay(200)
+          .duration(200)
+          .style('opacity', 0.9);
+      })
+      .on('mouseleave', function () {
+        tooltip.transition()
+          .duration(200)
+          .style('opacity', 0)
+          .each('end', function () {
+            d3.select(this).style('display', 'none');
+          });
+      });
+  };
+}
