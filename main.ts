0d6ac0a0a461ca29547b465c39078c006ecf5068
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-12-08 11:35:18 +0100

    fix adding of dom element in tooltip

--- main.ts
+++ main.ts
@@ -6,33 +6,39 @@
 /// <amd-dependency path='css!./style' />
 import d3 = require('d3');
 
-const tooltip = d3.select('body').append('div')
-  .attr('class', 'tooltip')
-  .style('display', 'block')
-  .style('opacity', 0);
 
-
+function getTooltip() {
+  var t = d3.select('body > div.tooltip');
+  if (t.empty()) {
+    t = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('display', 'block')
+      .style('opacity', 0);
+  }
+  return t;
+}
 /**
  * returns a D3 compatible call method, which registers itself to show a tooltip upon mouse enter
  * @param toLabel the text to show or a function to determine the text to show
  * @returns {Function}
  */
-export function bind<T>(toLabel:((d:T, i:number)=>string) | string) {
+export function bind<T>(toLabel:((d:T, i:number)=>string) | string, delay = 200) {
   //wrap as function
   const labelfor:((d:T, i:number)=>string) = <any>d3.functor(toLabel);
 
   return function (selection:d3.Selection<T>) {
     selection.on('mouseenter.tooltip', function (d:T, i) {
-        tooltip
-          .html(labelfor.call(this, d, i))
+        const tooltip = getTooltip();
+        tooltip.html(labelfor.call(this, d, i))
           .style('left', (d3.event.pageX + 5) + 'px')
           .style('top', (d3.event.pageY - 28) + 'px');
         tooltip.style('display', 'block').transition()
-          .delay(200)
+          .delay(delay)
           .duration(200)
           .style('opacity', 0.9);
       })
       .on('mouseleave.tooltip', function () {
+        const tooltip = getTooltip();
         tooltip.transition()
           .duration(200)
           .style('opacity', 0)
